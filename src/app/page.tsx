7b132e132467da6cb224495514c94bff41017776
Alberto Enriquez <136874006+EnriquezAlberto@users.noreply.github.com>
2023-08-02 20:01:53 -0300

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,57 @@
-"use client"
-import { useChat } from 'ai/react';
+"use client";
 
-function Homepage() {
-const {handleInputChange, handleSubmit, messages} = useChat()
+import { useChat } from "ai/react";
 
-{messages.map(m => (
-  <div className='bg-slate-300'
-  key={m.id}>{m.content}</div>
-))}
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit, isLoading } =
+    useChat();
+  console.log(messages);
 
   return (
     <section className="flex justify-center items-center h-screen">
-      <form className="max-w-xl w-full" onSubmit={handleSubmit}>
-        {/* MENSAJE */}
+      <form onSubmit={handleSubmit} className="max-w-xl w-full">
+        <div className="text-white max-h-96 h-full overflow-y-auto">
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className={`flex flex-col mb-2 p-2 rounded-md ${
+                m.role === "assistant"
+                  ? "self-end bg-gray-800"
+                  : "self-start bg-blue-700"
+              }`}
+            >
+              <span
+                className={`text-xs ${
+                  m.role === "assistant" ? "text-right" : "text-left"
+                }`}
+              >
+                {m.role}
+              </span>{" "}
+              {m.content}
+            </div>
+          ))}
+        </div>
 
         <div className="flex justify-between my-4">
-          <label className="text-white block font-bold my-2">Di algo...</label>
-          <button className="bg-blue-600 text-white px-3 py-2 rounded-md">
-            ENVIAR
+          <label className="text-white block font-bold my-2">
+            Say something...
+          </label>
+          <button
+            className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50"
+            disabled={isLoading || !input}
+          >
+            Send
           </button>
         </div>
         <textarea
           rows={4}
-          placeholder="Escriba su mensaje aqui"
-          className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none"
+          value={input}
           onChange={handleInputChange}
-        ></textarea>
+          className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+          placeholder="Type something..."
+          autoFocus
+        />
       </form>
     </section>
   );
 }
-
-export default Homepage;
